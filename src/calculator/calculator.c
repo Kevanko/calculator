deea8835500d04fafca4d80a8c8bbd64bf3ed07e
Николаенков Михаил Дмитриевич <iv222s16@bwp01.csc.local>
2023-05-05 15:23:59 +0700

    da

--- src/calculator/calculator.c
+++ src/calculator/calculator.c
@@ -1,8 +1,28 @@
+#include <stdlib.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 #include <libcalculator/header.h>
 
-int main(){
-    printf("123\n");
+int main()
+{
+    char str[MAX_LENGTH];
+    fgets(str, MAX_LENGTH, stdin);
+    str[strlen(str) - 1] = '\0';
+
+    double num;
+
+    for (size_t i = 0; i < strlen(str); i++) {
+        if (str[i] >= '0' && str[i] <= '9') {
+            char* end;
+            num = strtod(&str[i], &end);
+            printf("%f ", num);
+            i = end - str;
+        } else
+            printf("%c", str[i]);
+    }
+    printf("\n");
+
     return 0;
 }
--- src/libcalculator/header.h
+++ src/libcalculator/header.h
@@ -1,6 +1,20 @@
 #pragma once
+#define MAX_LENGTH 4096
 
-typedef struct Stack {
-  void *value;
-  struct Stack *next;
-} Stack;
+typedef struct StackInt {
+    int value;
+    struct StackInt* next;
+} StackInt;
+
+typedef struct StackChar {
+    char value;
+    struct StackChar* next;
+} StackChar;
+
+StackInt* init_stack_int(int value);
+StackInt* push_int(StackInt* stack, int value);
+StackInt* pop_int(StackInt* stack);
+
+StackChar* init_stack_char(char value);
+StackChar* push_char(StackChar* stack, char value);
+StackChar* pop_char(StackChar* stack);
